d0ae9be9e17b94decbb92a7963258b1799721cde
tiagoraminelli <108493070+tiagoraminelli@users.noreply.github.com>
2022-11-04 00:43:50 -0300

    Add files via upload

---
+++ IT - DESAFIO 18/js/modo.js
@@ -0,0 +1,3 @@
+let body= document.querySelector("body");
+let botonColorMode= document.querySelector(".color-mode");
+botonColorMode.addEventListener("click" , ()=>{body.classList.toggle("dark-mode");})
